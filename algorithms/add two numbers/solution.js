1682d1793a4075c6d59f8a9da595701584b89aa8
syed-basit-ali <basit.shah94@gmail.com>
2017-01-29 13:47:53 +0500

    solution of Add Two Numbers using javascript

---
+++ algorithms/add two numbers/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([3, 3], 6));
+
+function twoSum(nums, target) {
+  for (let row = 0; row < nums.length; row++) {
+    for (let col = row; col < nums.length; col++) {
+      if (col !== row) {
+        const sum = nums[row] + nums[col];
+        if (sum === target) {
+          return [row, col];
+        }
+      }
+    }
+  }
+
+  return [];
+}
