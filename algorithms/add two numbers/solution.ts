9027373cb7347328291fc202e65dc2a137c2e97a
syed-basit-ali <basit.shah94@gmail.com>
2017-01-30 15:54:42 +0500

    Add Two Number using javascript implemented

---
+++ algorithms/add two numbers/solution.ts
@@ -0,0 +1,43 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let l1String = "";
+  let l2String = "";
+
+  while (l1 !== null) {
+    l1String = l1.val + l1String;
+    l1 = l1.next;
+  }
+
+  while (l2 !== null) {
+    l2String = l2.val + l2String;
+    l2 = l2.next;
+  }
+
+  const sum: string = (BigInt(l1String) + BigInt(l2String)).toString();
+
+  let head: ListNode | null = null;
+  for (let i = sum.length - 1; i >= 0; i--) {
+    const newNode: ListNode = new ListNode(~~sum[i]);
+    if (head) {
+      let current = head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+      continue;
+    }
+    head = newNode;
+  }
+  return head;
+}
