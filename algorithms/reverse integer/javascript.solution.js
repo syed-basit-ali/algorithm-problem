656002f56721b503228551576fe00932b1dec6af
syed-basit-ali <basit.shah94@gmail.com>
2017-02-04 16:31:51 +0500

    Algorithm: reverse, Language: Javascript

---
+++ algorithms/reverse integer/javascript.solution.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+
+function reverse(x) {
+  const regex = /\d+/;
+  let sign;
+  let modified = "";
+
+  const strNumber = x.toString();
+
+  for (let row = strNumber.length - 1; row >= 0; row--) {
+    if (strNumber[row].match(regex)) {
+      modified += strNumber[row];
+      continue;
+    }
+    sign = strNumber[row];
+  }
+
+  let finalNumber;
+  if (sign) {
+    finalNumber = `${sign}${modified}`;
+  } else {
+    finalNumber = modified;
+  }
+
+  const higherLimit = Math.pow(2, 31);
+  const lowerLimit = Math.pow(-2, 31);
+
+  if (finalNumber > higherLimit || finalNumber < lowerLimit) {
+    return 0;
+  } else {
+    return finalNumber;
+  }
+}
