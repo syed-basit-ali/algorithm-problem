27252160a795f2f172963391238ea17219d4ea85
syed-basit-ali <basit.shah94@gmail.com>
2017-02-13 17:02:28 +0500

    Algorithm: generateParenthesis, Language: Javascript

---
+++ algorithms/generate parentheses/javascript.solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+
+var generateParenthesis = function (n) {
+  const result = [];
+
+  const backtrack = (s, left, right) => {
+    if (s.length === 2 * n) {
+      result.push(s);
+      return;
+    }
+
+    if (left < n) {
+      backtrack(s + "(", left + 1, right);
+    }
+
+    if (right < left) {
+      backtrack(s + ")", left, right + 1);
+    }
+  };
+
+  backtrack("", 0, 0);
+
+  return result;
+};
