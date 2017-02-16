cad94a95062ba336ed7f4efe8e282397bbd21775
syed-basit-ali <basit.shah94@gmail.com>
2017-02-16 19:48:22 +0500

    Algorithm: sumOfLeftLeaves, Language: Javascript

---
+++ algorithms/sum of left leaves/javascript.solution.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+var sumOfLeftLeaves = function (root) {
+  const iterate = (node, isLeft) => {
+    if (!node) return 0;
+
+    if (!node.left && !node.right && isLeft) return node.val;
+
+    return iterate(node.left, true) + iterate(node.right, false);
+  };
+
+  return iterate(root, false);
+};
